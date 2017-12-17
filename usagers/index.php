76f58384e65b6724789ea1ffd289e7e7b6bd42e4
Jules Lassara <jules.lssr@gmail.com>
2017-12-17 13:27:44 +0100

    Animation hover icons

--- usagers/index.php
+++ usagers/index.php
@@ -34,20 +34,106 @@ if(!isConnected()) {
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
 
-          <div class="card" style="width: 20rem;">
-            <div class="card-block">
-              <h4 class="card-title">Nom Prénom</h4>
+          <div class="card-deck">
+            <div class="card">
+              <div class="card-block">
+                <h4 class="card-title">Nom Prénom</h4>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-venus"></i>
+                        <span class="popup-text">Sexe</span>
+                      </td>
+                      <td>Femme</td>
+                    </tr>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-map-marker"></i>
+                        <span>Adresse</span>
+                      </td>
+                      <td>10 rue du Moulin - 31400 Toulouse</td>
+                    </tr>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-calendar-plus-o"></i>
+                        <span>Date et lieu de naissance</span>
+                      </td>
+                      <td>3 Mars 1987 - Paris</td>
+                    </tr>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-id-card-o"></i>
+                        <span>Numéro de sécurité sociale</span>
+                      </td>
+                      <td>1234 5678 9101</td>
+                    </tr>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-user-circle-o"></i>
+                        <span>Médecin référent</span>
+                      </td>
+                      <td>Docteur Nom Prénom</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">
+                  <a href="#" class="card-link"><i class="fa fa-pencil"></i> Modifier</a>
+                  <a href="#" class="card-link"><i class="fa fa-times-circle-o"></i> Supprimer</a>
+                </small>
+              </div>
             </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><i class="fa fa-venus"></i> Femme</li>
-              <li class="list-group-item"><i class="fa fa-map-marker"></i> 10 rue du Moulin - 31400 Toulouse</li>
-              <li class="list-group-item"><i class="fa fa-calendar-plus-o"></i> 3 Mars 1987 - Paris</li>
-              <li class="list-group-item"><i class="fa fa-id-card-o"></i> 1234 5678 9101</li>
-              <li class="list-group-item"><i class="fa fa-user-circle-o"></i> Docteur Nom Prénom</li>
-            </ul>
-            <div class="card-block">
-              <a href="#" class="card-link">Modifier</a>
-              <a href="#" class="card-link">Supprimer</a>
+            <div class="card">
+              <div class="card-block">
+                <h4 class="card-title">Nom Prénom</h4>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-venus"></i>
+                        <span class="popup-text">Sexe</span>
+                      </td>
+                      <td>Femme</td>
+                    </tr>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-map-marker"></i>
+                        <span>Adresse</span>
+                      </td>
+                      <td>10 rue du Moulin - 31400 Toulouse</td>
+                    </tr>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-calendar-plus-o"></i>
+                        <span>Date et lieu de naissance</span>
+                      </td>
+                      <td>3 Mars 1987 - Paris</td>
+                    </tr>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-id-card-o"></i>
+                        <span>Numéro de sécurité sociale</span>
+                      </td>
+                      <td>1234 5678 9101</td>
+                    </tr>
+                    <tr>
+                      <td class="icon-row-card bull_over">
+                        <i class="fa fa-user-circle-o"></i>
+                        <span>Médecin référent</span>
+                      </td>
+                      <td>Docteur Nom Prénom</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">
+                  <a href="#" class="card-link"><i class="fa fa-pencil"></i> Modifier</a>
+                  <a href="#" class="card-link"><i class="fa fa-times-circle-o"></i> Supprimer</a>
+                </small>
+              </div>
             </div>
           </div>
 
